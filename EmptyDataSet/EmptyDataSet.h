ca6ff7265e2957cad13cbc8ebfc5d3d4ee962c5a
Mx <LoadingMx@126.com>
2020-11-22 10:43:07 +0800

    Initial Commit

---
+++ EmptyDataSet/EmptyDataSet.h
@@ -0,0 +1,18 @@
+//
+//  EmptyDataSet.h
+//  EmptyDataSet
+//
+//  Created by Mx on 2020/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EmptyDataSet.
+FOUNDATION_EXPORT double EmptyDataSetVersionNumber;
+
+//! Project version string for EmptyDataSet.
+FOUNDATION_EXPORT const unsigned char EmptyDataSetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmptyDataSet/PublicHeader.h>
+
+
